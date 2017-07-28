ac5ff4d5e339dc1435e21710ad3c3ffd492ff715
张振峰 <xyls_ok@126.com>
2017-07-28 11:07:09 +0800

    test

---
+++ src/main/java/com/todo/memo/dao/UserDao.java
@@ -0,0 +1,28 @@
+package com.todo.memo.dao;
+
+import com.todo.memo.model.User;
+import com.todo.memo.tables.records.UserRecord;
+import org.jooq.Result;
+import org.springframework.stereotype.Component;
+
+import static com.todo.memo.Tables.USER;
+
+/**
+ * Created by zhangzf on 17/7/28.
+ */
+@Component
+public class UserDao extends JooqDao<UserRecord,User,Integer>{
+    protected UserDao() {
+        super(USER, User.class);
+    }
+
+    @Override
+    protected Integer getId(User user) {
+        return user.getId();
+    }
+
+    public User getUserByUsername(String userName,String password) {
+        UserRecord userRecord = create().selectFrom(USER).where(USER.USER_NAME.eq(userName)).and(USER.PASSWORD.eq(password)).fetchOne();
+        return null!=userRecord?userRecord.into(User.class):null;
+    }
+}
---
+++ src/main/java/com/todo/memo/ui/UserWeb.java
@@ -0,0 +1,63 @@
+package com.todo.memo.ui;
+
+import com.todo.memo.dao.UserDao;
+import com.todo.memo.inter.MemoService;
+import com.todo.memo.model.Memo;
+import com.todo.memo.model.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by zhangzf on 17/3/22.
+ */
+@Api(value = "用户管理", description = "用户管理")
+@RestController
+@RequestMapping(value = "/user")
+public class UserWeb {
+
+    @Autowired
+    MemoService memoService;
+
+    @Autowired
+    UserDao userDao;
+
+    @ApiOperation(value = "添加", notes = "添加")
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public void add(HttpServletRequest request, @RequestBody User user) {
+        userDao.save(user);
+    }
+
+    @ApiOperation(value = "修改", notes = "修改")
+    @RequestMapping(value = "/update", method = RequestMethod.PUT)
+    public void update(@RequestBody User user) {
+        userDao.update(user);
+    }
+
+
+    @ApiOperation(value = "查询", notes = "查询")
+    @RequestMapping(value = "/detail/{id}", method = RequestMethod.GET)
+    public User get(@PathVariable("id") Integer id) {
+        return userDao.findById(id);
+    }
+
+    @ApiOperation(value = "Login", notes = "Login")
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public User login(HttpServletRequest request, @RequestBody User user) {
+        if (null != user) {
+            User u = userDao.getUserByUsername(user.getUserName(), user.getPassword());
+            if (null != u) {
+                u.setPassword("");
+                request.getSession().setAttribute(u.getUserName(), u);
+                return u;
+            }
+        }
+        return null;
+    }
+
+}
