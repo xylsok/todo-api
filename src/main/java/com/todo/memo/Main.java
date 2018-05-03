4695bdbb6b2b93b966b3582818b00cc7792ccd93
张振峰 <xyls_ok@126.com>
2018-05-03 09:49:54 +0800

    t

--- src/main/java/com/todo/memo/Main.java
+++ src/main/java/com/todo/memo/Main.java
@@ -3,6 +3,7 @@ package com.todo.memo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 // import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 // import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
@@ -12,7 +13,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 @EnableConfigurationProperties()
 // @EnableFeignClients
-// @EnableEurekaClient
+ @EnableEurekaClient
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
